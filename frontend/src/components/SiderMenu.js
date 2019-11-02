6468fdd32b9678b2e1b5fd9785516fb75c73249c
Luiz <luiz.felipe.556@outlook.com>
2019-11-02 20:29:55 -0300

    styles

--- frontend/src/components/SiderMenu.js
+++ frontend/src/components/SiderMenu.js
@@ -6,40 +6,40 @@ const { Sider } = Layout;
 
 const SiderMenu = () => {
     return (
-        <Layout>
-    <Sider
-      breakpoint="lg"
-      collapsible={false}
-      onBreakpoint={broken => {
-        console.log(broken);
-      }}
-      width='450px'
-    >
-      <div className="logo" />
-      <Menu 
-      theme="dark"
-      mode="inline"
-      defaultSelectedKeys={['4']}
-      
+    <Layout>
+      <Sider
+        breakpoint="lg"
+        collapsible={false}
+        onBreakpoint={broken => {
+          console.log(broken);
+        }}
+        width='22vw'
       >
-        <Menu.Item key="1">
-          <Icon type="user" />
-          <span className="nav-text">nav 1</span>
-        </Menu.Item>
-        <Menu.Item key="2">
-          <Icon type="video-camera" />
-          <span className="nav-text">nav 2</span>
-        </Menu.Item>
-        <Menu.Item key="3">
-          <Icon type="upload" />
-          <span className="nav-text">nav 3</span>
-        </Menu.Item>
-        <Menu.Item key="4">
-          <Icon type="user" />
-          <span className="nav-text">nav 4</span>
-        </Menu.Item>
-      </Menu>
-    </Sider>
+        <div className="logo" />
+        <Menu 
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={['4']}
+        style={{ backgroundColor: 'rgba(4, 4, 102,0.4)', height: '100vh'}}
+        >
+          <Menu.Item key="1" style={{ marginTop: '230px', fontSize: '35px', height: '60px'}}>
+            <Icon type="user" />
+            <span className="nav-text">nav 1</span>
+          </Menu.Item>
+          <Menu.Item key="2" style={{ marginTop: '100px', fontSize: '35px', height: '60px'}}>
+            <Icon type="video-camera" />
+            <span className="nav-text">nav 2</span>
+          </Menu.Item>
+          <Menu.Item key="3" style={{ marginTop: '100px', fontSize: '35px', height: '60px'}}>
+            <Icon type="upload" />
+            <span className="nav-text">nav 3</span>
+          </Menu.Item>
+          <Menu.Item key="4" style={{ marginTop: '100px', fontSize: '35px', height: '60px'}}>
+            <Icon type="user" />
+            <span className="nav-text">nav 4</span>
+          </Menu.Item>
+        </Menu>
+      </Sider>
     </Layout>
     );
 }
