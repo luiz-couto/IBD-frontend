701cb1bcd50b693d4300f5d0d45c3ac3d5ff0fd2
Luiz <luiz.felipe.556@outlook.com>
2019-11-03 10:15:46 -0300

    style for side menu

--- frontend/src/components/SiderMenu.js
+++ frontend/src/components/SiderMenu.js
@@ -2,9 +2,34 @@ import React from 'react';
 import { Layout, Menu, Icon } from 'antd';
 import "antd/dist/antd.css";
 
+import './styles.css';
+
 const { Sider } = Layout;
 
-const SiderMenu = () => {
+const selectedStyle = {
+  marginTop: '100px',
+  fontSize: '55px',
+  height: '130px',
+  width: '40vw',
+}
+
+const notSelectedStyle = {
+  marginTop: '100px',
+  fontSize: '35px',
+  height: '60px'
+}
+
+class SiderMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        one_selected: false,
+        two_selected: false,
+        three_selected: false,
+        four_selected: false
+    };
+  } 
+  render() {
     return (
     <Layout>
       <Sider
@@ -20,21 +45,29 @@ const SiderMenu = () => {
         theme="dark"
         mode="inline"
         defaultSelectedKeys={['4']}
-        style={{ backgroundColor: 'rgba(4, 4, 102,0.4)', height: '100vh'}}
+        style={{ backgroundColor: 'rgba(4, 4, 102,0.4)', height: '100vh' }}
         >
-          <Menu.Item key="1" style={{ marginTop: '230px', fontSize: '35px', height: '60px'}}>
+          <Menu.Item className='customclass' key="1" style={this.state.one_selected ? selectedStyle : notSelectedStyle}onClick={() => {
+            this.setState({ one_selected: true, two_selected: false, three_selected: false, four_selected: false })
+          }}>
             <Icon type="user" />
             <span className="nav-text">nav 1</span>
           </Menu.Item>
-          <Menu.Item key="2" style={{ marginTop: '100px', fontSize: '35px', height: '60px'}}>
+          <Menu.Item className='customclass' key="2" style={this.state.two_selected ? selectedStyle : notSelectedStyle} onClick={() => {
+            this.setState({ one_selected: false, two_selected: true, three_selected: false, four_selected: false })
+          }}>
             <Icon type="video-camera" />
             <span className="nav-text">nav 2</span>
           </Menu.Item>
-          <Menu.Item key="3" style={{ marginTop: '100px', fontSize: '35px', height: '60px'}}>
+          <Menu.Item className='customclass' key="3" style={this.state.three_selected ? selectedStyle : notSelectedStyle}onClick={() => {
+            this.setState({ one_selected: false, two_selected: false, three_selected: true, four_selected: false })
+          }}>
             <Icon type="upload" />
             <span className="nav-text">nav 3</span>
           </Menu.Item>
-          <Menu.Item key="4" style={{ marginTop: '100px', fontSize: '35px', height: '60px'}}>
+          <Menu.Item className='customclass' key="4" style={this.state.four_selected ? selectedStyle : notSelectedStyle}onClick={() => {
+            this.setState({ one_selected: false, two_selected: false, three_selected: false, four_selected: true })
+          }}>
             <Icon type="user" />
             <span className="nav-text">nav 4</span>
           </Menu.Item>
@@ -42,6 +75,7 @@ const SiderMenu = () => {
       </Sider>
     </Layout>
     );
+  }
 }
 
 export default SiderMenu;
