436c25272cecdfd2c63bf708ee459973f9fe4d0b
Luiz <luiz.felipe.556@outlook.com>
2019-11-11 11:29:13 -0300

    Created right menu

--- frontend/src/pages/SearchCountry/index.js
+++ frontend/src/pages/SearchCountry/index.js
@@ -4,7 +4,7 @@ import SiderMenu from '../../components/SiderMenu';
 import './styles.css';
 
 import analyze from 'rgbaster';
-import { AutoComplete, Layout, Input, Icon, Typography, Col, Spin } from 'antd';
+import { AutoComplete, Layout, Input, Icon, Typography, Col, Spin, Menu } from 'antd';
 
 import { query } from "../../utils/database/query";
 import { relative } from 'path';
@@ -20,7 +20,8 @@ class SearchCountry extends React.Component{
             loading: true,
             countryImage: "",
             countryList: [],
-            selectedCountry: 'Brazil'
+            selectedCountry: 'Brazil',
+            current: 'search'
         };
     }
 
@@ -68,6 +69,13 @@ class SearchCountry extends React.Component{
         this.setState({ color: result[0].color, loading: false });
     }
 
+    handleClick = e => {
+        console.log('click ', e);
+        this.setState({
+          current: e.key,
+        });
+    };
+
     render() {
         let { loading, color } = this.state;
         let countryImageData = "data:image/jpeg;base64," + this.state.countryImage;
@@ -89,22 +97,40 @@ class SearchCountry extends React.Component{
                     <SiderMenu color={color}/>
                     <Layout>
                         <Content>
+                            <div style={{ width: '10vw', float: 'right', marginTop: '4vh', height: '10vh' }}>
+                            <Menu onClick={this.handleClick} selectedKeys={this.state.current} mode='vertical-right' theme='dark' >
+                                <Menu.Item key='search' style={{  }} >
+                                    <Icon type='search' />
+                                    Search
+                                </Menu.Item>
+                           
+                            <Menu.Item key='info'  style={{ }}>
+                                <Icon type="appstore" />
+                                    About It
+                                </Menu.Item>
+                            </Menu>
+                            </div>
                         <Col className='container'>
-                            <Text className='title'>{this.state.selectedCountry}</Text>
-                            <AutoComplete
-                                dataSource={this.state.countryList}
-                                className='autoComplete'
-                                dropdownClassName="certain-category-search-dropdown"
-                                onSelect={(countryName) => {
-                                    this.searchCountry(countryName)}}
-                                onSearch={() => {}}
-                                size='large'
-                                filterOption={(inputValue, option) =>
-                                    option.props.children.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
-                                }
-                            >
-                            <Input suffix={<Icon type="search" style={{ marginRight: 20, fontSize: 20 }}/>} />
-                            </AutoComplete>
+                            { this.state.current == 'search' ?
+                            <>
+                                <Text className='title'>{this.state.selectedCountry}</Text>
+                                <AutoComplete
+                                    dataSource={this.state.countryList}
+                                    className='autoComplete'
+                                    dropdownClassName="certain-category-search-dropdown"
+                                    onSelect={(countryName) => {
+                                        this.searchCountry(countryName)}}
+                                    onSearch={() => {}}
+                                    size='large'
+                                    filterOption={(inputValue, option) =>
+                                        option.props.children.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+                                    }
+                                >
+                                <Input suffix={<Icon type="search" style={{ marginRight: 20, fontSize: 20 }}/>} />
+                                </AutoComplete>
+                            </> :
+                            <span>BLA</span>
+                            }
                         </Col>
                     </Content>
                     </Layout>
