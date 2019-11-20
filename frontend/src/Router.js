d5c3fc23c394736556b1d9168958a01d37e0ee72
Luiz <luiz.felipe.556@outlook.com>
2019-11-20 13:50:38 -0300

    redirect path "/" to search

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import SearchCountry from './pages/SearchCountry';
 import CommunicationAndInformation from './pages/Communication_and_Information';
@@ -14,6 +14,9 @@ const privateRoutes = [
 const Routes = () => {
     return (
       <Router>
+          <Route exact path="/" component={() => (
+          <Redirect to={{ pathname: '/search' }} />
+        )} />
           <Switch>
               {privateRoutes.map((route, index) => (
                   <InternalRoute {...route} key={index} />
